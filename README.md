6925fbd03680bd4340af47dc27528b1f81b07faa
HANNAH YI <hannahyi@uw.edu>
2022-11-18 16:55:05 -0800

    added files

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ There are 3 files:
 - .rmd
 - .html
 
+# The URL for a website hosted with GitHub Pages
+https://GITHUB_USERNAME.github.io/REPO_NAME
+
 This is practice for A4 so make sure to follow along carefully.
---
+++ analysis.R
@@ -0,0 +1,14 @@
+# Load the data.
+# Compute metrics of interest.
+# Generate data visualizations to display.
+
+# .R file- algorithmic work
+# .Rmd- create visuals
+
+# save key values & charts to variables so they can be referenced
+# in the index.Rmd file!
+
+# Load Libraries
+library(dplyr)
+library(rworldmap) # easy mapping
+library(RColorBrewer) # color palette
